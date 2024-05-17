c773c80fab3636bf77b4505ad388ac53bfab58c4
Zirdgal <zirdman@gmail.com>
2024-05-17 21:43:06 +0300

    new stats

--- data/data.js
+++ data/data.js
@@ -1,7 +1,3 @@
 var game0000 = {team1: "", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, team2: "", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0};
-export var game0004 = {team1: "Brazz", team1Goal: 3,team1Assist: 0,team1Save: 3,team1Foul: 3, team2: "Sperma", team2Goal: 2,team2Assist: 0,team2Save: 8,team2Foul: 2};
-export var game0005 = {team1: "Sperma", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0,};
-
-
-let score = document.getElementById("match0005-text"); // Match0005 text thingy
-
+export var game0004 = {team1: "Brazz", team1Goal: 3,team1Assist: 0,team1Save: 3,team1Foul: 3, team2: "sperma", team2Goal: 2,team2Assist: 0,team2Save: 8,team2Foul: 2};
+export var game0005 = {team1: "sperma", team1Goal: 3,team1Assist: 0,team1Save: 0,team1Foul: 0, team2: "Faze", team2Goal: 4,team2Assist: 0,team2Save: 0,team2Foul: 0,};
