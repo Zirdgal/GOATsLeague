076d766ece4d6fb15e4ea4a36353bfdfb1e2ca2f
Zirdgal <zirdman@gmail.com>
2024-05-20 23:31:30 +0300

    New games

--- data/data.js
+++ data/data.js
@@ -21,6 +21,14 @@ export const game0007 = {
     team1: "McD", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, 
     team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0
 };
+export const game0008 = {
+    team1: "Sperma", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, 
+    team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0
+};
+export const game0009 = {
+    team1: "Brazz", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, 
+    team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0
+};
 
 const games = [
     game0004, game0005, game0006, game0007
