76460e455731e2be9692e781d7264cb6bdbca22d
Zirdgal <zirdman@gmail.com>
2024-05-20 23:31:45 +0300

    new games

---
+++ pages/matches/match-0008.js
@@ -0,0 +1,4 @@
+import { game0008 } from "../../data/data.js";
+import { displayMatchScore } from "./match-functions.js";
+
+displayMatchScore(game0008, "match0008-text"); // Display match score for game0004
---
+++ pages/matches/match-0009.js
@@ -0,0 +1,4 @@
+import { game0009 } from "../../data/data.js";
+import { displayMatchScore } from "./match-functions.js";
+
+displayMatchScore(game0009, "match0009-text"); // Display match score for game0004
