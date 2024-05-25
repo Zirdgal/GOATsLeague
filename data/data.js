8bb39f713b23c926348346ced61f05fad45790c4
Zirdgal <zirdman@gmail.com>
2024-05-26 02:20:20 +0300

    new videos and saves leader

--- data/data.js
+++ data/data.js
@@ -26,8 +26,8 @@ export const game0009 = {
     team2: "Sperma", team2Goal: 4,team2Assist: 0,team2Save: 0,team2Foul: 0
 };
 export const game0010 = {
-    team1: "Brazz", team1Goal: 5,team1Assist: 0,team1Save: 0,team1Foul: 0, 
-    team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 0,team2Foul: 0
+    team1: "Brazz", team1Goal: 5,team1Assist: 2,team1Save: 6,team1Foul: 0, 
+    team2: "Faze", team2Goal: 0,team2Assist: 0,team2Save: 12,team2Foul: 2
 };
 export const game0011 = {
     team1: "Brazz", team1Goal: 0,team1Assist: 0,team1Save: 0,team1Foul: 0, 
